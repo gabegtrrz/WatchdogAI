98595b7e72dab2dd1a7be6c8e4cbd444f296cc10
Gabriel Louis Gutierrez <gabe.gutz361@gmail.com>
2025-02-18 15:34:31 +0800

    5% dones realtime_data

--- data/realtime_data.py
+++ data/realtime_data.py
@@ -3,4 +3,18 @@ import numpy as np
 import random
 from datetime import datetime, timedelta
 
-def generate_realtime_price_data
+def generate_realtime_price_data (items, numdays=365):
+    price_data = []
+    start_date = datetime.now() - timedelta(days=numdays)
+
+    base_prices = {
+        # base prices per item
+        # EXAMPLES ONLY
+        'Laptop': 800,
+        'Projector': 300,
+        'Books': 20,
+        'Software License': 150,
+        'Printer': 200
+    }
+
+
