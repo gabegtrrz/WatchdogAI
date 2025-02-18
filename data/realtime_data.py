b6ca572d892820eae67af128c3ba3d5a571a3ad2
Gabriel Louis Gutierrez <gabe.gutz361@gmail.com>
2025-02-18 19:12:21 +0800

    realtime_data 90% done. Items and base price research remaining

--- data/realtime_data.py
+++ data/realtime_data.py
@@ -17,4 +17,32 @@ def generate_realtime_price_data (items, numdays=365):
         'Printer': 200
     }
 
-    
+    # Volatility factor (adjust for overall price fluctuation)
+    volatility = 0.05  # 5% volatility
+
+    for item in items:
+        # output:
+        # columns = items
+        # rows = daily price data with volatility
+
+        current_price = base_prices[item] * (1+random.uniform(-volatility, volatility))
+        price_history = [current_price]
+        
+        for i in range(numdays):
+            date= start_date + timedelta(days=i)
+
+            price_change = price_history[-1] * random.uniform(-volatility/2, volatility/2)
+            current_price = price_history[-1] + price_change
+            
+            # ensure current_price is non-zero and non-negative
+            if current_price <=0:
+                current_price = price_history[-1]
+
+            #add to price_history
+            price_history.append(current_price)
+            price_data.append([item, date.strftime('%Y-%m-%d'), round(current_price,2)])
+
+    df = pd.DataFrame(price_data, columns= ['item', 'date', 'price'])
+    return df
+
+
