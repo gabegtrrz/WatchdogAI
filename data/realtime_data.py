1bd8225c6f55b2c784d97d19966695e2264cb2f6
Jisi04 <jc.pacardo04@gmail.com>
2025-02-20 22:37:38 +0800

    item and price changes

--- data/realtime_data.py
+++ data/realtime_data.py
@@ -10,7 +10,7 @@ def generate_realtime_price_data (items, numdays=365):
     base_prices = {
         # base prices per item
         # BELOW ARE ONLY EXAMPLES 
-        'Cambridge IGCSE Physics: Student book': 11393.02,
+        'Physics: Student book': 11393.02,
         'Acer Aspire 5 A515-56G-551P': 36997,
         'Casio fx-991EX Scientific Calculator': 1259.46,
         'Canon PIXMA G3010 Printer': 8795,
@@ -18,7 +18,7 @@ def generate_realtime_price_data (items, numdays=365):
         'Modern Periodic Table Of The Elements (Size 12X17.5 Inches)': 38,
         'Magnetic Whiteboard 30 x 40cm': 149,
         'VEVOR Rotating World Globe with Stand 13 in/330.2 mm Diameter': 2757.58,
-        'MedPride Powder-Free Nitrile Exam Gloves, Iris Blue, Medium, Box/100': 571.96,
+        'Exam Gloves Box/100': 571.96,
         'Uvex Ultravision Goggles': 41794.49
     }
 
