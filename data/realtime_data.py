07f737feec8936f53d18616f59c5c7ac39cd85c8
Gabriel Louis Gutierrez <gabe.gutz361@gmail.com>
2025-02-24 09:44:30 +0800

    added item list to realtime

--- data/realtime_data.py
+++ data/realtime_data.py
@@ -7,7 +7,20 @@ def generate_realtime_price_data (items, numdays=365):
     price_data = []
     start_date = datetime.now() - timedelta(days=numdays)
 
-    items = [] # add the items here bro from data_generator
+    items = [
+        "Compound Microscope (1000x)", "Refracting Telescope (70mm)", "Beaker (50ml)",
+        "Beaker (100ml)", "Beaker (250ml)", "Beaker (500ml)", "Test Tube (10ml)",
+        "Test Tube (20ml)", "Bunsen Burner", "Triple Beam Balance",
+        "Microscope Slides (box of 50)", "Microscope Coverslips (box of 100)",
+        "Dissecting Kit (basic)", "Sodium Chloride (NaCl) - Solid (1kg)",
+        "Hydrochloric Acid (HCl) - 1M (1L)", "Lab Manual - Grade 10 Science",
+        "Reflecting Telescope (150mm)", "Digital Spectrophotometer", "High-Precision Balance",
+        "Potassium Iodide (KI) - Solid (500g)",
+        "AmScope M150B-LED Replacement LED Bulb", "Beaker (1000ml)",
+        "Replacement Xenon Flash Lamp for Do PerkinElmer])",
+        "Sulfuric Acid (H2SO4) - 1M (1L)"
+        ]
+    
     base_prices = {
         # base prices per item
         # BELOW ARE ONLY EXAMPLES 
