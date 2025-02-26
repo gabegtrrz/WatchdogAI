81facc65f8ec001a57ac8d533dc025c6d8da38b8
Gabriel Louis Gutierrez <gabe.gutz361@gmail.com>
2025-02-26 16:58:09 +0800

    renamed data_gen to sim_transactions_generator

--- data/sim_transactions_generator.py
+++ data/sim_transactions_generator.py
@@ -33,26 +33,7 @@ def generate_blockchain_data(num_transactions = 1000, base_prices = BASE_PRICES)
 
     # need to edit this below: update the item values accordingly
     
-    ''' WE NEED TO PUT THIS IN PROCUREMENT_DATA.PY
-    procurement_methods = {
-    "Competitive Bidding": {
-        "items": ["Compound Microscope (1000x)", "Refracting Telescope (70mm)", "Beaker (50ml)", "Beaker (100ml)", "Beaker (250ml)", "Beaker (500ml)", "Test Tube (10ml)", "Test Tube (20ml)", "Bunsen Burner", "Triple Beam Balance", "Microscope Slides (box of 50)", "Microscope Coverslips (box of 100)", "Dissecting Kit (basic)", "Sodium Chloride (NaCl) - Solid (1kg)", "Hydrochloric Acid (HCl) - 1M (1L)", "Lab Manual - Grade 10 Science"],
-        "frequency": 0.5
-    },
-    "Limited Source Bidding": {
-        "items": ["Reflecting Telescope (150mm)", "Digital Spectrophotometer", "Data Analysis Software (Logger Pro)", "High-Precision Balance"],
-        "frequency": 0.2
-    },
-    "Negotiated Procurement": {
-        "items": ["Potassium Iodide (KI) - Solid (500g)", "AmScope M150B-LED Replacement LED Bulb", "Beaker (1000ml)"],
-        "frequency": 0.2
-    },
-    "Direct Contracting": {
-        "items": ['Calibration Service (for high-precision balance)', "Replacement Xenon Flash Lamp for Do PerkinElmer", "Sulfuric Acid (H2SO4) - 1M (1L)"], 
-        "frequency": 0.1
-    }
-    }
-    '''
+
 
     for i in range(num_transactions):
         # timestamp object converted into str for immutability
