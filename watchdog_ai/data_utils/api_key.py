185e95872f58d90903c570cad41b920ba5c67462
Jisi04 <jc.pacardo04@gmail.com>
2025-04-06 20:53:22 +0800

    API key

--- watchdog_ai/data_utils/api_key.py
+++ watchdog_ai/data_utils/api_key.py
@@ -1,1 +1,10 @@
 SCRAPEOPS_API_KEY = 'da295174-4da2-4dfb-9841-5e7a8a291599'
+
+
+#   new Api key
+#   da295174-4da2-4dfb-9841-5e7a8a291599
+#   e02f5404-cae6-484a-bab6-244c6f829e54
+
+
+#   old Api key
+#   39460d69-2a10-482d-bd64-5e96132d14f1
