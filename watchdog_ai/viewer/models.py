b6c300d54660252b1f441824f3c4344013c2f4c6
Gabriel Louis Gutierrez <gabe.gutz361@gmail.com>
2025-04-07 15:16:13 +0800

    Enhance BlockchainTransactionData model with additional fields and improve string representation

--- watchdog_ai/viewer/models.py
+++ watchdog_ai/viewer/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Transaction(models.Model):
     transaction_id = models.CharField(max_length=50, unique=True)
@@ -14,6 +15,47 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f"{self.transaction_id} - {self.item_name}"
+
+# --------------------------
+
+class BlockchainTransactionData(models.Model):
+    # Core Blockchain fields
+
+    block_hash = models.CharField(max_length=64, primary_key=True)
+    block_number = models.IntegerField(db_index=True, unique=True, help_text="Sequential block number (index).") 
+    previous_hash = models.CharField(max_length=64, db_index=True)
+    timestamp = models.DateTimeField(db_index=True, default=timezone.now)
+
+    # Stored Transaction Data (used for hashing and retrieval). storing the exact data used for hashing to ensure verification works reliably
+    transaction_data_json = models.JSONField(help_text="The transaction data exactly as it was hashed (JSON format).") 
+
+    ### Mirror fields for fast querying ###
+    transaction_id_query = models.IntegerField(db_index=True, null=True, blank=True)
+    item_name_query = models.CharField(max_length=255, db_index=True, null=True, blank=True)
+    supplier_query = models.CharField(max_length=100, db_index=True, null=True, blank=True)
+    transaction_date_query = models.DateField(db_index=True, null=True, blank=True)
+    procurement_method_query = models.CharField(db_index=True, max_length=50, null=True, blank=True,) #change later to procurement_method_id
+
+    def __str__(self):
+        # Ensure transaction_id exists in the JSON before accessing
+        tid = self.transaction_data_json.get('transaction_id', 'N/A') 
+        return f"Block {self.block_number} (Tx ID: {tid})"
+    
+    class Meta:
+        ordering = ['block_number'] # Default ordering when fetching multiple blocks
+        verbose_name = "Blockchain Record"
+        verbose_name_plural = "Blockchain Records"
+
+
+
+    
+
+
+
+
+
+
+# --------------------------
 
 class Anomaly(models.Model):
     transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
